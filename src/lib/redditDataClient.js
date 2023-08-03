7dd214d5b3e9a8c7351607f1554bcd74bd10a7fd
anvaka <anvaka@gmail.com>
2023-08-02 22:39:27 -0700

    updated data

--- src/lib/redditDataClient.js
+++ src/lib/redditDataClient.js
@@ -2,7 +2,7 @@ import createRedditDataClient from "./createRedditDataClient";
 
 const isProd = true;
 const dataEndpoint = isProd
-  ? "https://anvaka.github.io/sayit-data/2/"
+  ? "https://anvaka.github.io/sayit-data/3/"
   : "http://localhost:8081/";
 const redditDataClient = createRedditDataClient(dataEndpoint);
 
